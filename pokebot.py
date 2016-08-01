37d384551d31995eb4830bb07cbf7afd2c82be4c
Jirayu Sakulyuenyong <jay.cpe23.ku@gmail.com>
2016-08-01 16:29:27 -0700

    fix get_GPX error

--- pokebot.py
+++ pokebot.py
@@ -94,7 +94,7 @@ def main():
 
     if not api.login(config.auth_service, config.username, config.password, config.cached):
         return
-    get_GPX()
+    api.get_GPX()
     while True:
         try:
             api.main_loop()
